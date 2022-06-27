f214aaf4354e43390671f40fe8acbe47b3904e0c
Andrew Christie <awchristie86@gmail.com>
2022-06-26 22:44:09 -0400

    support filtering by date

--- cmd/ewmd/ewmd.go
+++ cmd/ewmd/ewmd.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
 	"github.com/achristie/save2db/pkg/platts"
 )
@@ -12,13 +13,19 @@ func main() {
 	APIKey := flag.String("apikey", "NULL", "API Key to call API with")
 	Username := flag.String("username", "NULL", "Username to get a token")
 	Password := flag.String("password", "NULL", "Password associated with Username")
-	// StartDate := flag.String("t", time.Now().UTC().AddDate(0, 0, -3).Format("2006-01-02T15:04:05"), "Get updates since date. Format 2006-01-02T15:04:05")
-	// PageSize := flag.Int("p", 1000, "The page size to use for API Calls. Max is 10,000")
+	StartDate := flag.String("t", time.Now().UTC().AddDate(0, 0, -3).Format("2006-01-02T15:04:05"), "Get updates since date. Format 2006-01-02T15:04:05")
+	PageSize := flag.Int("p", 1000, "The page size to use for API Calls. Max is 10,000")
 	flag.Parse()
 
 	client := platts.NewClient(APIKey, Username, Password)
 
-	td, err := client.GetEWindowMarketData()
+	// initial parameters
+	start, err := time.Parse("2006-01-02T15:04:05", *StartDate)
+	if err != nil {
+		log.Fatal("Could not parse time", err)
+	}
+
+	td, err := client.GetEWindowMarketData(start, 1, *PageSize)
 	if err != nil {
 		log.Fatal(err)
 	}
--- pkg/platts/ewmd_impl.go
+++ pkg/platts/ewmd_impl.go
@@ -1,15 +1,17 @@
 package platts
 
 import (
+	"fmt"
 	"net/url"
+	"strconv"
+	"time"
 )
 
-func (c *Client) GetEWindowMarketData() (EWindowMarketData, error) {
+func (c *Client) GetEWindowMarketData(StartTime time.Time, Page int, PageSize int) (EWindowMarketData, error) {
 	params := url.Values{}
-	// params.Add("subscribed_only", "true")
-	// params.Add("Facet.Field", "mdc")
-	// params.Add("Field", "symbol")
-	params.Add("PageSize", "2")
+	params.Add("filter", fmt.Sprintf("update_time >= %q", StartTime.Format("2006-01-02T15:04:05")))
+	params.Add("pagesize", strconv.Itoa(PageSize))
+	params.Add("page", strconv.Itoa(Page))
 
 	req, err := c.newRequest("tradedata/v3/ewindowdata", params)
 
